9ecc992125551c115ad6b00ea3547dfbc1178175
Etta Rapp <ettirapp@gmail.com>
2020-12-25 15:59:02 -0500

    Update print_fsm.c

--- print_fsm.c
+++ print_fsm.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
+// This function prints all the transitions in an FSM.
+// It is used by the print function in the interactive debugger.
 void printFsm(int num_states, int fsm_states[], int fsm[50][52])
 {
   int state_name;
   char input;
   int transition = 0;
   
+  // Iterate through the fsm matrix
   for (int row = 0; row < num_states; row++)
     {
       state_name = fsm_states[row];
       for (int col = 0; col < 52; col++)
 	{
+	  // If this transition is defined, print it
 	  if (fsm[row][col] != -1)
 	    {
+	      // Calculate the input corresponding to this column
 	      if (col >= 0 && col <= 25)
 		{
 		  input = (char) col + 97;
@@ -22,6 +27,7 @@ void printFsm(int num_states, int fsm_states[], int fsm[50][52])
 		  input = (char) col + 39;
 		}
 	      printf("transition %d: state %d with input %c goes to state %d\n", transition, state_name, input, fsm[row][col]);
+	      // increment the number of transitions
 	      transition++;
 	    }
 	}
